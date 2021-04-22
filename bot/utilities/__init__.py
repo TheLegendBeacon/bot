2fdbdd4e55fa0594e099ebf1c91d45023f25b74b
Virdin <73127621+VirdanTheBurden@users.noreply.github.com>
2021-04-22 12:43:35 -0400

    condense spacing

--- bot/utilities/__init__.py
+++ bot/utilities/__init__.py
@@ -6,7 +6,6 @@ def get_config_val(yaml_file: str, *strings: str) -> dict:
     return_dict = {}
 
     for string in strings:
-
         sep_string = "".join([" " if char in punctuation else char for char in string]).split()
 
         with open(yaml_file) as config:
@@ -14,9 +13,8 @@ def get_config_val(yaml_file: str, *strings: str) -> dict:
             data = yaml.load(config, Loader=yaml.UnsafeLoader)
 
             for key in sep_string:
-
+                
                 if key in data:
-
                     data = data[key]
 
                     if key == sep_string[-1]:
