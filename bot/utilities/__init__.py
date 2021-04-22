aa8e546b4c1a781984e9d19f4ef5e71e3a2793d1
Virdin <73127621+VirdanTheBurden@users.noreply.github.com>
2021-04-22 12:30:18 -0400

    Create __init__.py

---
+++ bot/utilities/__init__.py
@@ -0,0 +1,28 @@
+def get_config_val(*strings: str) -> dict:
+
+    import yaml
+    from string import punctuation
+
+    return_dict = {}
+
+    for string in strings:
+
+        sep_string = "".join([" " if char in punctuation else char for char in string]).split()
+
+        with open("config.yaml") as config:
+
+            data = yaml.load(config, Loader=yaml.UnsafeLoader)
+
+            for key in sep_string:
+
+                if key in data:
+
+                    data = data[key]
+
+                    if key == sep_string[-1]:
+                        return_dict[key] = data
+
+                else:
+                    raise yaml.YAMLError(f'Key "{key}" cannot be found')
+
+    return return_dict
