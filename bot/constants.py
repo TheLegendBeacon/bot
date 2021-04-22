b3da317a78675ead0e0437d77ca1a48ab9682646
hypergamer80 <79152594+hypergamer80@users.noreply.github.com>
2021-04-22 13:29:49 -0300

    added negative replies as a constant

--- bot/constants.py
+++ bot/constants.py
@@ -14,6 +14,18 @@ BOT_REPO_URL = "https://github.com/cat-dev-group/bot"
 MESSAGE_LIMIT = 2000
 
 
+NECATIVE_REPLIES = [
+    "Nah mate, this ain't gonna work.",
+    "**MEOW** nope **MEOW**",
+    "Unpoggers.",
+    "DA DIVINE CAT says you can't.",
+    "Not in a million cat years.",
+    "Yeah, no.",
+    "<command cannot be executed>",
+    "Error code: 3120"
+]
+
+
 class Channels:
     devlog = 809125747037306960
     modlog = 834500035982786620
