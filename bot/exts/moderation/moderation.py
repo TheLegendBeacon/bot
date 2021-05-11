6c32ef2e5fd7edea862e86105b29214dc99ca8d6
Ankith Abhayan <ankithabhayan@gmail.com>
2021-05-11 15:53:53 +0100

    code changed to make bot send dm embed to the person getting infracted

--- bot/exts/moderation/moderation.py
+++ bot/exts/moderation/moderation.py
@@ -45,6 +45,22 @@ class Moderation(commands.Cog):
             await ctx.send("You can't ban yourself!")
             return
 
+        #gets current date and time; converts it with colons and slashes between then to make sense
+        current_time = datetime.now()
+        current_time = current_time.strftime("%H:%M:%S")
+
+        current_date = datetime.date()
+        current_date = current_date.strftime("%d/%m/%y")
+
+        #dm embed made here
+        dm_message = discord.Embed(
+            title = "**Infraction**: Ban",
+            description = f"You just got banned from the `{ctx.guild}` server.\n`reason:` {reason}\n`date and time:` {current_time} {current_date}",
+            color = colors["light_blue"]
+        )
+
+        await user.dm_channel.send(embed=dm_message)
+
         await ctx.guild.ban(user)
         await ctx.send(f"Successfully banned {user.name}")
 
@@ -105,6 +121,22 @@ class Moderation(commands.Cog):
         )
 
         await user.add_roles(muted_role)
+
+        #gets current date and time; converts it with colons and slashes between then to make sense
+        current_time = datetime.now()
+        current_time = current_time.strftime("%H:%M:%S")
+
+        current_date = datetime.date()
+        current_date = current_date.strftime("%d/%m/%y")
+
+        #dm embed made here
+        dm_message = discord.Embed(
+            title = "**Infraction**: Mute",
+            description = f"You just got muted from the `{ctx.guild}` server.\n`duration:` until {unmute_time}\n`reason:` {reason}\n`date and time:` {current_time} {current_date}",
+            color = colors["light_blue"]
+        )
+
+        await user.dm_channel.send(embed=dm_message)
 
         json_input = {user.id: unmute_time.timestamp()}
         try:
